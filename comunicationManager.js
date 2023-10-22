ec6a870a2eff2b251a2b108f51469cbdace09afc
julsluks <a22betvilver@inspedralbes.cat>
2023-10-22 21:36:48 +0200

    Vercel fetch

--- comunicationManager.js
+++ comunicationManager.js
@@ -1,5 +1,5 @@
 export async function getMovies() {
-    let response = await fetch("http://www.omdbapi.com/?s=cars&apikey=8c3be63e");
+    let response = await fetch("https://www.omdbapi.com/?s=cars&apikey=8c3be63e");
     let movies = await response.json();
     console.log(movies);
     return movies.Search;
